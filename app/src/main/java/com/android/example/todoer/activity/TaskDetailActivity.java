8289479c05da3207cfa6e9ecbc17353c2cc8dd01
RomanRudyi <roma1995101@gmail.com>
2017-02-22 12:39:19 +0200

    Added TaskDetailActivity, TaskListAdapter, Realm classes Task & Project Create interaction between RecyclerView's items and TaskDetail via interface Listener

---
+++ app/src/main/java/com/android/example/todoer/activity/TaskDetailActivity.java
@@ -0,0 +1,26 @@
+package com.android.example.todoer.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.android.example.todoer.R;
+import com.android.example.todoer.model.TaskDummy;
+
+public class TaskDetailActivity extends AppCompatActivity {
+
+    public static String EXTRA_TASK_NO = "taskNo";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_detail);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        int taskNo = (Integer) getIntent().getExtras().getInt(EXTRA_TASK_NO);
+
+        TextView taskDetailTextView = (TextView) findViewById(R.id.taskDetailTextView);
+        taskDetailTextView.setText(TaskDummy.tasks[taskNo]);
+    }
+}
---
+++ app/src/main/java/com/android/example/todoer/adapter/TaskListAdapter.java
@@ -0,0 +1,65 @@
+package com.android.example.todoer.adapter;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.android.example.todoer.R;
+
+public class TaskListAdapter extends RecyclerView.Adapter<TaskListAdapter.ViewHolder> {
+
+    private String[] tasks;
+    private Listener listener;
+
+    public TaskListAdapter(String[] tasks) {
+        this.tasks = tasks;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        CardView cardView = (CardView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_task, parent, false);
+        return new ViewHolder(cardView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        CardView cardView = holder.cardView;
+        TextView taskTextView = (TextView) cardView.findViewById(R.id.taskTitleTextView);
+        taskTextView.setText(tasks[position]);
+
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    listener.onClick(position);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return tasks.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private CardView cardView;
+
+        public ViewHolder(CardView cardView) {
+            super(cardView);
+            this.cardView = cardView;
+        }
+    }
+
+    public static interface Listener {
+        public void onClick(int position);
+    }
+
+    public void setListener(Listener listener) {
+        this.listener = listener;
+    }
+}
